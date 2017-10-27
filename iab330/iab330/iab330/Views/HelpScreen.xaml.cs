83c6c38dc5928dc02eb5cb27374e876b612081cc
David Nguyen <david.nguyen161@gmail.com>
2017-10-28 01:05:13 +1000

    Fixed help page, page is now scrollable

--- iab330/iab330/iab330/Views/HelpScreen.xaml.cs
+++ iab330/iab330/iab330/Views/HelpScreen.xaml.cs
@@ -14,12 +14,9 @@ namespace iab330.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class HelpScreen : ContentPage
 	{
-		public HelpScreen ()
-		{
-			InitializeComponent ();
-
-            var helpText = new Label { Text = "" };
-
+        public HelpScreen()
+        {
+            InitializeComponent();
             var assembly = typeof(HelpScreen).GetTypeInfo().Assembly;
             Stream stream = assembly.GetManifestResourceStream("iab330.help.txt");
             string text = "";
@@ -28,16 +25,7 @@ namespace iab330.Views
                 text = reader.ReadToEnd();
             }
             helpText.Text = text;
-
-            Content = new StackLayout
-            {
-                Padding = new Thickness(20, 20, 20, 20),
-                VerticalOptions = LayoutOptions.StartAndExpand,
-                Children = {
-                     new Label { FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
-                     FontAttributes = FontAttributes.Bold }, helpText 
-                }
-            };
+            
         }
 	}
 }
