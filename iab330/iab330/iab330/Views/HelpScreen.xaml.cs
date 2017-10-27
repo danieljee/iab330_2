738c725e552799cf181adbfdf11ee575bd8cad43
David Nguyen <david.nguyen161@gmail.com>
2017-10-27 18:48:55 +1000

    Added text to help screen and fixed up column widths

--- iab330/iab330/iab330/Views/HelpScreen.xaml.cs
+++ iab330/iab330/iab330/Views/HelpScreen.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +17,27 @@ namespace iab330.Views
 		public HelpScreen ()
 		{
 			InitializeComponent ();
-		}
+
+            var helpText = new Label { Text = "" };
+
+            var assembly = typeof(HelpScreen).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("iab330.help.txt");
+            string text = "";
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+            }
+            helpText.Text = text;
+
+            Content = new StackLayout
+            {
+                Padding = new Thickness(20, 20, 20, 20),
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                Children = {
+                     new Label { FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
+                     FontAttributes = FontAttributes.Bold }, helpText 
+                }
+            };
+        }
 	}
 }
